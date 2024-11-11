5197a9fe308242a1291ba6b608413662e4bf8f68
Muhammed Fayi P <mhdfaaiz803@gmail.com>
2024-11-11 22:05:16 +0400

    final final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./Components/NavBar";
 import About from "./Pages/AboutPage";
 import Project from "./Pages/Project";
 import Contact from "./Pages/Contact";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 function App() {
   return (
